2e4bf719fb4969d4b77bd7587c8dfd4057dab2c1
juanoc7 <90334649+juanoc7@users.noreply.github.com>
2025-06-03 15:47:34 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+import requests
+import time
+from telegram import Bot
+import schedule
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def get_eth_price():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT"
+    response = requests.get(url)
+    data = response.json()
+    return float(data['price'])
+
+def send_telegram_message(bot, message):
+    bot.send_message(chat_id=CHAT_ID, text=message)
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+def job():
+    price = get_eth_price()
+    message = f"💰 El precio actual de ETH/USDT es: {price} USD"
+    send_telegram_message(bot, message)
+
+schedule.every().hour.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
